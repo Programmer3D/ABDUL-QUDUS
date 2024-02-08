0dd2a2418d8bdb9421040d8368dff6ce5c5be540
Programmer3D <125832775+Programmer3D@users.noreply.github.com>
2024-02-08 04:09:09 -0800

    Add files via upload

---
+++ ASS.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $asa = 5;
+        $i = 0;
+        $da = 1.3;
+        $d = 0.0;
+        $str = "ELI";
+
+        echo $asa + $i . "<br>";
+        echo $da + $d ."<br>";
+        echo "Your name is ". $str;
+
+
+
+
+
+    ?>
+</body>
+</html>
